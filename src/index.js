85b0aafcf83e3e9a8850990db05c7c844a3892f8
ZikaZaki <zika.github@gmail.com>
2022-09-28 01:38:01 +0300

    Add node-scss library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 // Importing components
 import TodoContainer from './components/TodoContainer';
+
+// Importing CSS
+import "./App.css";
 
 
 ReactDOM.render(
