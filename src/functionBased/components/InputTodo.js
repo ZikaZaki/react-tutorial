8518fd12050e0be3482b458f8ec6696f7f6d10da
ZikaZaki <zika.github@gmail.com>
2022-09-30 00:38:25 +0300

    Refactor InputTodo.js to functional component

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,53 +1,42 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class InputTodo extends Component {
-    state = {
-        title: ""
-    }
+const InputTodo = props => {
+    const [inputText, setInputText] = useState({
+        title: "",
+    })
 
-    onChange = e => {
-        this.setState({
+    const onChange = e => {
+        setInputText({
+            ...inputText,
             [e.target.name]: e.target.value,
-        });
-    };
-/* For Handling React form that has more than one text input do that:
-onChange = e => {
-    this.setState({
-        [e.target.name]: e.target.value
-    });
-};
-and add a 'name' attribute to each of the input tags & assign it 
-a value of the state property name. For this we have: name="title" 
-*/
-
-    handleSubmit = e => {
-        e.preventDefault();
-        if(this.state.title.trim()){
-            this.props.addTodoProps(this.state.title);
-            this.setState({
-                title: ""
-            });
-        }else {
-            alert("Please write item");
-        }
-        
-    };
-
-    render() {
-        return (
-            <form className="form-container" onSubmit={this.handleSubmit}>
-                <input 
-                    type="text"
-                    className="input-text"
-                    placeholder="Add Todo..." 
-                    value={this.state.title}
-                    name="title"
-                    onChange={this.onChange} 
-                />
-                <button className="input-submit">Submit</button>
-            </form>
-        )
+        })
     }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (inputText.title.trim()) {
+            props.addTodoProps(inputText.title)
+            setInputText({
+                title: "",
+            })
+        } else {
+            alert("Please write item")
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <input 
+                type="text"
+                className="input-text"
+                placeholder="Add todo..."
+                value={inputText.title}
+                name="title"
+                onChange={onChange} 
+            />
+            <button className="input-submit">Submit</button>
+        </form>
+    )
 }
 
 export default InputTodo;
